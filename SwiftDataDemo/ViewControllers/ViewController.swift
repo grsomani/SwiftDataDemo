385ad4c289306ea3495774883e456e85326b10b0
Ganesh Somani <ganesh.somani@gmail.com>
2023-10-28 23:21:50 +0530

    Added Dummy alert for list creation with empty methods

--- SwiftDataDemo/ViewControllers/ViewController.swift
+++ SwiftDataDemo/ViewControllers/ViewController.swift
@@ -11,9 +11,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupNavigationBar()
     }
 
-
+    private func setupNavigationBar() {
+        self.title = "ToDo List"
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewToDoList))
+        self.navigationItem.rightBarButtonItem = addButton
+    }
+    
+    @objc private func addNewToDoList() {
+        let alertController = UIAlertController(title: "New ToDo List", message: "What is this todo list regarding?", preferredStyle: .alert)
+        alertController.addTextField(configurationHandler: { textField in
+            textField.placeholder = "Enter List Title here"
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let createAction = UIAlertAction(title: "Create", style: .default) { _ in
+            // create new model here
+            // navigate to new screen for list creation
+        }
+        alertController.addAction(cancelAction)
+        alertController.addAction(createAction)
+        self.navigationController?.present(alertController, animated: true)
+    }
 }
 
