4428763a022fb60bbe5d25c1975a53b8ccb211d6
Ganesh Somani <ganesh.somani@gmail.com>
2023-10-28 22:40:21 +0530

    Initial Commit

---
+++ SwiftDataDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftDataDemo
+//
+//  Created by Ganesh Somani on 28/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
