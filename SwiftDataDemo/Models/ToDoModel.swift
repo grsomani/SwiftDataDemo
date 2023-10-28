c3a4ca1bfdc948475c7f6cdf65c3a4e50f389756
Ganesh Somani <ganesh.somani@gmail.com>
2023-10-28 22:57:04 +0530

    Added ToDoModel to store basic ToDo list data

---
+++ SwiftDataDemo/Models/ToDoModel.swift
@@ -0,0 +1,35 @@
+//
+//  ToDoModel.swift
+//  SwiftDataDemo
+//
+//  Created by Ganesh Somani on 28/10/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class ToDoData {
+    var title: String
+    var createdDate: Date
+    var lastEditedDate: String?
+    var list: [ToDoTask]?
+    
+    init(title: String, createdDate: Date, lastEditedDate: String? = nil, list: [ToDoTask]?) {
+        self.title = title
+        self.createdDate = createdDate
+        self.lastEditedDate = lastEditedDate
+        self.list = list
+    }
+}
+
+@Model
+class ToDoTask {
+    var taskTitle: String
+    var completed: Bool
+    
+    init(taskTitle: String, completed: Bool = false) {
+        self.taskTitle = taskTitle
+        self.completed = completed
+    }
+}
